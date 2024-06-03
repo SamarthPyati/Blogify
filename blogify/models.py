9a597511969192e829a6b0642a8695646c439a60
Samarth Sanjay Pyati <samarthpyati@gmail.com>
2024-06-03 20:44:22 +0530

    Update name run.py -> app.py

--- blogify/models.py
+++ blogify/models.py
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
-    def get_reset_token(self, expires_sec: int=1800):
+    def get_reset_token(self, expires_sec: int=1800): 
         s = Serializer(app.config['SECRET_KEY'], expires_in=expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
 
